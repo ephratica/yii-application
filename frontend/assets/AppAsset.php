8f334a990c6f11dc82ea6ed8972ca624ced9e08c
ephratica <ephratica@foxmail.com>
2023-02-05 13:32:39 +0800

    added assets path

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,8 +13,12 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/world-map.css',
     ];
     public $js = [
+        'js/d3.v4.js',
+        'js/d3-geo-projection.v2.min.js',
+        'js/d3-scale-chromatic.v1.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
