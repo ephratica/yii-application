10a0f5cab5012cc16d13e4c156fc1df7247d8615
alittle_f <gd020301@163.com>
2023-02-09 00:43:13 +0800

    delete line chart

--- frontend/views/sanction-type/index.php
+++ frontend/views/sanction-type/index.php
@@ -16,67 +16,6 @@ $this->title = '各国对俄罗斯的制裁（类型）';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="sanction-type-index">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= ECharts::widget([
-    'responsive' => true,
-    'options' => [
-        'style' => 'height: 400px;'
-    ],
-    'pluginEvents' => [
-        'click' => [
-            new JsExpression('function (params) {console.log(params)}'),
-            new JsExpression('function (params) {console.log("ok")}')
-        ],
-        'legendselectchanged' => new JsExpression('function (params) {console.log(params.selected)}')
-    ],
-    'pluginOptions' => [
-        'option' => [
-            'title' => [
-                'text' => 'Energy Price Fluctuation',
-                'textStyle' =>[
-                    'fontFamily' => 'Center'
-                ],
-            ],
-            'tooltip' => [
-                'trigger' => 'axis'
-            ],
-            'legend' => [
-                'data' => ['Natural Gas', 'Wti Crude','Brent Crude', 'Low Sulphur Gas Oil', 'Soybean Oil','Uls Diesel','Gasoline']
-            ],
-            'grid' => [
-                'left' => '3%',
-                'right' => '4%',
-                'bottom' => '3%',
-                'containLabel' => true
-            ],
-            'toolbox' => [
-                'feature' => [
-                    'saveAsImage' => []
-                ]
-            ],
-            'xAxis' => [
-                'name' => 'type',
-                'type' => 'category',
-                'boundaryGap' => false,
-                'data' => $type
-            ],
-            'yAxis' => [
-                'type' => 'value'
-            ],
-            'series' => [
-                [
-                    'name' => 'value',
-                    'type' => 'line',
-                    'stack' => 'PRICE',
-                    'data' => $value
-                ]
-            ]
-        ]
-    ]
-]); ?>
-</div>
 
 <div class="aid-type-bar">
     <?= ECharts::widget([
